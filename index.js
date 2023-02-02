45543045ca285bea12e5189fc1c75eb8b2a741a1
jkunciene <j.kunciene@gmail.com>
2023-02-02 14:20:30 +0200

    get, post

--- index.js
+++ index.js
@@ -13,9 +13,33 @@ const products = [
 ]
 
 //svarbu 1.sukurti route
+//visus produktu gavimas
 app.get('/api/products', (req, res) => {
     res.send(products);
 });
+
+//konkrecios prekes paieska
+//req yra prekes id
+//res aprasome patys
+app.get('/api/products/:id', (req, res) => {
+    const my_product = products.find(product => product.id === parseInt(req.params.id));
+    if(!my_product) res.status(404).send("not found");
+    res.send(my_product);
+ });
+
+ //naujos prekes pridejimas, irasymas
+ //be patikrinimu, ar netuscia reiksme siunciama ir tt...
+
+ app.post('/api/products', (req, res) => {
+    const product = {
+        id: products.length +1,
+        title: req.body.title
+    };
+
+    products.push(product);
+    res.send(products);
+ });
+
 
 //apsirasome port'a ant kurio veiks serveris
 const PORT = 5001;
